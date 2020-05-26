9f3c99dc91cb6bb4cc70296f9ba5665ee128dd3a
unknown <fayssaloudbib@gmail.com>
2020-05-26 19:41:54 +0200

    adding comments to bind function

--- bind.js
+++ bind.js
@@ -1,3 +1,10 @@
+/**
+ * Takes a function 'fn' and binds it to some arguments 'boundArgs', then applies those arguments and others 'args' in some context.
+ * @param {*} fn : entry function to bind
+ * @param {*} context : context to apply 'fn' within
+ * @param  {...any} boundArgs 
+ * @returns : a function which is bound version of 'fn'
+ */
 const bind = (fn, context, ...boundArgs) => (...args) => fn.apply(context, [...boundArgs, ...args]);
 
 function fn(context, args) {
